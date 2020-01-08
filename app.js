53caacaa9969b65d8c0b747bab35f0e19330bc21
Diego <diegohn.dev@gmail.com>
2020-01-08 09:37:23 -0600

    Fixed bug at mewtwo image

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //jscs:disable
 let doorImage1 = document.querySelector('#door1');
-const mewTwo = 'https://dhna32.github.io/Chore-Door/mew.svg'; //botdoorpath
+const mewTwo = 'https://dhna32.github.io/Chore-Door/mewtwo.svg'; //botdoorpath
 let doorImage2 = document.querySelector('#door2');
 const pikachu = 'pikachu.png';
 let doorImage3 = document.querySelector('#door3');
