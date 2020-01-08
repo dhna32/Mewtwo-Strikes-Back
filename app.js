25e451b9061ed0cd3de5f6c2e7d4f35aa87dde64
Diego <diegohn.dev@gmail.com>
2020-01-08 09:23:45 -0600

    Project Finished

--- app.js
+++ app.js
@@ -2,37 +2,115 @@
 
 //jscs:disable
 let doorImage1 = document.querySelector('#door1');
-const mewTwo = 'mewtwo.svg'; //botdoorpath
+const mewTwo = 'file:///home/diego/Documents/CODE/CodeCademy/Unit6/Chore-Door/mewtwo.svg'; //botdoorpath
 let doorImage2 = document.querySelector('#door2');
 const pikachu = 'pikachu.png';
 let doorImage3 = document.querySelector('#door3');
-const mew= 'mew.svg';
-const numClosedDoors = 3;
+const mew = 'mew.svg';
+const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
+const startButton = document.querySelector('#start');
+let currentlyPlaying = true;
 
 doorImage1.onclick = () => {
-  doorImage1.src = mewTwo;
+  if(!isClicked(doorImage1) && currentlyPlaying === true){
+    doorImage1.src = openDoor1;
+    playDoor(doorImage1);
+  }
 };
 
 doorImage2.onclick = () => {
-  doorImage2.src = pikachu;
+  if(!isClicked(doorImage2) && currentlyPlaying === true){
+    doorImage2.src = openDoor2;
+    playDoor(doorImage2);
+  }
 };
 
 doorImage3.onclick = () => {
-  doorImage3.src = mew;
+  if(!isClicked(doorImage3) && currentlyPlaying === true){
+    doorImage3.src = openDoor3;
+    playDoor(doorImage3);
+  }
+};
+
+startButton.onclick = () =>{
+  if(!currentlyPlaying){
+    startRound();
+  }
+
+};
+
+const startRound = ()  => {
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  numClosedDoors = 3;
+  startButton.innerHTML = 'Good Luck!';
+  currentlyPlaying = true;
+  randomMewtwoDoor();
+};
+
+
+
+const gameOver = (status) =>{
+  if(status === 'win'){
+    startButton.innerHTML = 'You win! Play Again?';
+  } else{
+    startButton.innerHTML = 'Game Over! Play Again?';
+  }
+  currentlyPlaying = false;
 };
 
 const randomMewtwoDoor = () => {
   let choreDoor = Math.floor(Math.random()* numClosedDoors);
+  //console.log(choreDoor);
   if (choreDoor === 0) {
     openDoor1 = mewTwo;
+    openDoor2 = pikachu;
+    openDoor3 = mew;
   }else if (choreDoor === 1) {
+    openDoor1 = mew;
     openDoor2 = mewTwo;
+    openDoor3 = pikachu;
   } else if (choreDoor === 2){
     openDoor3 = mewTwo;
+    openDoor1 = pikachu;
+    openDoor2 = mew;
   } else {
     alert('SOMETHING HAS GONE WRONG!');
   }
 };
+
+
+  const playDoor = (door) => {
+    numClosedDoors--;
+    if(numClosedDoors === 0){
+      gameOver('win');
+    } else if(isMewTwo(door)){
+      gameOver();
+    }
+  };
+
+  const isClicked = (door) =>{
+    if (door.src === closedDoorPath) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const isMewTwo = (door) =>{
+    console.log(door);
+    console.log(door.src);
+    console.log(mewTwo);
+    if(door.src === mewTwo){
+      return true;
+    }else{
+      return false;
+    }
+  };
+
+  startRound();
